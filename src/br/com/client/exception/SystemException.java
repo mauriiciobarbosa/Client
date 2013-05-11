a9bb232127673ab1e6a880ef49cd748de94c5f0b
mauriiciobarbosa <mauriiciobarbosa@gmail.com>
2013-05-11 16:33:12 -0300

    First Commit

---
+++ src/br/com/client/exception/SystemException.java
@@ -0,0 +1,12 @@
+package br.com.client.exception;
+
+public class SystemException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SystemException(String message) {
+		super(message);
+	}
+}
